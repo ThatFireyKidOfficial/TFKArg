9db75e7cefd6f5dc8ed598f9f0e895c8f15e4d25
ThatFireyKidOfficial <emmaleejlocke@gmail.com>
2025-04-11 17:05:29 -0500

    Create scripts.js

---
+++ styles/scripts/scripts.js
@@ -0,0 +1,6 @@
+// Simple interaction example
+const secretPageBtn = document.getElementById("secretPageBtn");
+
+secretPageBtn.addEventListener("click", () => {
+    window.location.href = "secretPage.html";  // Redirect to a hidden page
+});
